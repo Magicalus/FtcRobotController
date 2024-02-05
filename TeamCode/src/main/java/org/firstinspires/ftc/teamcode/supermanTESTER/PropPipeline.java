3566d654e62ccd393a856a0110296403f171b3cf
Jo <jyoo3712@gmail.com>
2024-02-05 15:57:09 -0500

    super

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/supermanTESTER/PropPipeline.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/supermanTESTER/PropPipeline.java
@@ -20,9 +20,9 @@ public class PropPipeline implements VisionProcessor {
     public static int redLeftX = (int) (815);
     public static int redLeftY = (int) (550);
     public static int redCenterX = (int) (1365);
-    public static int redCenterY = (int) (775);
+    public static int redCenterY = (int) (655);
     public static int blueCenterX = (int) (240);
-    public static int blueCenterY = (int) (1425);
+    public static int blueCenterY = (int) (725);
     public static int blueLeftX = (int) (675);
     public static int blueLeftY = (int) (985);
     public static int centerWidth = (int) (175);
